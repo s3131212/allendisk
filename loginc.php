006c329937359a45383035b1ab69ba0aabe9c2c9
Allen Chou <s3131212@gmail.com>
2015-12-31 22:21:43 +0800

    remove the code used for updating from 1.4 to 1.5

--- loginc.php
+++ loginc.php
@@ -16,27 +16,6 @@ function login($username, $password)
 {
     $res = $GLOBALS['db']->select('user', array('name' => $username));
 
-    /* For update from 1.4 */
-    /* This code will be removed in 1.6 */
-    if (strlen($res[0]['pass']) == 32) {
-        if ($res[0]['pass'] == md5($password)) {
-            $GLOBALS['db']->update('user', array('pass' => password_hash($password, PASSWORD_DEFAULT)), array('name' => $username));
-            $passphrase['b'] = md5_128($password);
-            $iv = substr(md5("\x1B\x3C\x58".$passphrase['b'], true), 0, 8);
-            $key = substr(md5("\x2D\xFC\xD8".$passphrase['b'], true).md5("\x2D\xFC\xD9".$passphrase['b'], true), 0, 24);
-            foreach ($GLOBALS['db']->select('file', array('owner' => $username)) as $value) {
-                $passphrase['a'] = $value['secret'];
-                $passphrase['c'] = rtrim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $passphrase['a'], MCRYPT_MODE_CBC, $iv)), "\0\3");
-                $GLOBALS['db']->update('file', array('secret' => $passphrase['c']), array('id' => $value['id']));
-            }
-
-            return true;
-        } else {
-            return false;
-        }
-    }
-    /* Update Code End */
-
     if (password_verify($password, $res[0]['pass'])) {
         return true;
     } else {
