ca5278ba8257d01bae98e5de15a3eb41d4aee2be
Allen Chou <s3131212@gmail.com>
2017-10-04 22:45:24 +0800

    Fix #20 SSRF in remotedownload.php

--- remotedownload.php
+++ remotedownload.php
@@ -6,6 +6,12 @@ Author: Allen Chou ( http://allenchou.cc )
 License: MIT License
 */
 include 'config.php';
+require_once('class/safecurl/autoload.php');
+use fin1te\SafeCurl\SafeCurl;
+use fin1te\SafeCurl\Exception; 
+
+/* Start to develop here. Best regards https://php-download.com/ */
+
 if (!session_id()) {
     session_start();
 }
@@ -46,31 +52,40 @@ if (isset($_POST['file'])) {
             $result = 'totalout';
         }
     }
-    $file = @file_get_contents($_POST['file']);
-    $header = @get_headers($_POST['file'], 1);
-    if ($file !== false && stripos($header[0], '200') !== false) {
-        $name = getname($_POST['file']);
-        if (strlen($file) == 0) { // 只有當無法正常偵測大小時才使用 header ，因為header可能被偽造
-            $size = $header['Content-Length'];
-        } else {
-            $size = strlen($file);
-        }
-        if ($config['size'] != 0) {
-            if ($size > ($config['size'] * 1000 * 1000)) {
-                $result = 'sizeout';
-            }
-        }
+    try {
+        $curlHandle = curl_init();
+        $file = SafeCurl::execute($_POST['file'], $curlHandle);
+    } catch (Exception $e) {
+        $return = array(
+            'result' => 'nofile',
+            'id' => '0',
+            'name' => 'null',
+            'size' => 0
+        );
 
-        $filename = sha1(md5(mt_rand().uniqid()));
-        if ($result == '') {
-            $fp = fopen($_POST['file'], 'rb');
-            file_put_contents('./file/'.$filename.'.temp', file_get_contents($_POST['file']));
-            $mkid = sha1(mt_rand().uniqid());
-            $db->insert(array('name' => $name, 'size' => $size, 'owner' => $_SESSION['username'], 'id' => $mkid, 'secret' => '', 'realname' => $filename, 'type' => $header['Content-Type'], 'dir' => $_SESSION['dir'], 'recycle' => '0'), 'file');
-            $result = 'success';
-        }
+        echo json_encode($return);
+        exit();
+    }
+    $header = @get_headers($_POST['file'], 1);
+    $name = getname($_POST['file']);
+    if (strlen($file) == 0) { // 只有當無法正常偵測大小時才使用 header ，因為header可能被偽造
+        $size = $header['Content-Length'];
     } else {
-        $result = 'nofile';
+        $size = strlen($file);
+    }
+    if ($config['size'] != 0) {
+        if ($size > ($config['size'] * 1000 * 1000)) {
+            $result = 'sizeout';
+        }
+    }
+
+    $filename = sha1(md5(mt_rand().uniqid()));
+    if ($result == '') {
+        $fp = fopen($_POST['file'], 'rb');
+        file_put_contents('./file/'.$filename.'.temp', file_get_contents($_POST['file']));
+        $mkid = sha1(mt_rand().uniqid());
+        $db->insert(array('name' => $name, 'size' => $size, 'owner' => $_SESSION['username'], 'id' => $mkid, 'secret' => '', 'realname' => $filename, 'type' => $header['Content-Type'], 'dir' => $_SESSION['dir'], 'recycle' => '0'), 'file');
+        $result = 'success';
     }
     $return = array(
         'result' => $result,
