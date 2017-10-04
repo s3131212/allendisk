8ab305d1c4eae27b12be4a7fd7c1ee9c731f0833
Allen Chou <s3131212@gmail.com>
2017-10-04 18:18:18 +0800

    Fix #14

--- ajax/ajax_move_dir.php
+++ ajax/ajax_move_dir.php
@@ -10,13 +10,37 @@ include dirname(dirname(__FILE__)).'/config.php';
 if (!session_id()) {
     session_start();
 }
+
+function check_if_is_child($target_id, $ori_id){
+    if($target_id == $ori_id) return true;
+
+    $res = $GLOBALS['db']->select('dir', array('id' => $target_id));
+    while($res[0]['parent'] != '0'){
+        if($res[0]['parent'] == $ori_id){
+            return true;
+        }
+        $res = $GLOBALS['db']->select('dir', array('id' => $res[0]['parent']));
+    };
+
+    return false;
+}
+
 $res = $GLOBALS['db']->select('dir', array('id' => $_GET['id']));
 if ($_SESSION['login'] && $_SESSION['username'] == $res[0]['owner'] && isset($_GET['dir'])  && isset($_GET['id'])) {
-    $result = $GLOBALS['db']->update('dir', array('parent' => $_GET['dir']), array('id' => $_GET['id']));
-    echo json_encode(array(
-        'success' => $result,
-        'message' => $result ? '成功移動。' : '移動失敗。',
-    ));
+
+    if(!check_if_is_child($_GET['dir'], $_GET['id'])){
+        $result = $GLOBALS['db']->update('dir', array('parent' => $_GET['dir']), array('id' => $_GET['id']));
+        echo json_encode(array(
+            'success' => $result,
+            'message' => $result ? '成功移動。' : '移動失敗。',
+        ));
+    }else{
+        echo json_encode(array(
+            'success' => false,
+            'message' => '不可移動到自身或子資料夾。',
+        ));
+    }
+    
     $token = @touch(dirname(dirname(__FILE__)).'/updatetoken/'.md5($_SESSION['username']).'.token');
 } else {
     echo json_encode(array(
